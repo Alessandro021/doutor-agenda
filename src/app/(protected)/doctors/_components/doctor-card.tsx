d619a666f5a0c88495e1b9b94a45e0a92c75ae44
unknown <alessandrodamata6@gmail.com>
2025-06-17 20:23:02 -0300

    feat: Adiciona funcionalidade de exclusão de médicos com confirmação em diálogo. Implementa a ação de deletar médico e feedback visual ao usuário. Inclui novo componente de AlertDialog para gerenciar a confirmação da exclusão.

--- src/app/(protected)/doctors/_components/doctor-card.tsx
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -1,8 +1,22 @@
 "use client";
 
-import { CalendarIcon, ClockIcon, DollarSignIcon } from "lucide-react";
+import { CalendarIcon, ClockIcon, DollarSignIcon, Trash2 } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
 import { useState } from "react";
+import { toast } from "sonner";
 
+import { deleteDoctor } from "@/actions/delete-doctor";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -32,6 +46,25 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
     .map((name) => name[0])
     .join("");
   const availability = getAvailability(doctor);
+  const deleteDoctorAction = useAction(deleteDoctor, {
+    onSuccess: () => {
+      toast.success("Médico deletado com sucesso", {
+        description: "O médico foi deletado com sucesso",
+        position: "top-center",
+      });
+    },
+    onError: () => {
+      toast.error("Erro ao deletar médico", {
+        description: "Por favor, verifique suas credenciais e tente novamente.",
+        position: "top-center",
+      });
+    },
+  });
+
+  const handleDeleteDoctorClick = () => {
+    if (!doctor) return;
+    deleteDoctorAction.execute({ id: doctor?.id });
+  };
   return (
     <Card>
       <CardHeader>
@@ -62,7 +95,7 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
         </Badge>
       </CardContent>
       <Separator />
-      <CardFooter>
+      <CardFooter className="flex flex-col gap-2">
         <Dialog
           open={isUpsertingDoctorDialogOpen}
           onOpenChange={setIsUpsertingDoctorDialogOpen}
@@ -81,6 +114,31 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
             }}
           />
         </Dialog>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive" className="w-full">
+              <Trash2 className="h-4 w-4" />
+              Excluir médico
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Tem certeza que deseja excluir o médico?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Essa ação não pode ser desfeita. Isso irá excluir o médico e
+                todas as consultas agendadas com ele.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeleteDoctorClick}>
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </CardFooter>
     </Card>
   );
